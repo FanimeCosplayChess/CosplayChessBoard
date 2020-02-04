8c22a6e9f6f7628b5e8466a1e937af175ffd29b4
elliott <ellmill00@gmail.com>
2020-02-03 19:28:52 -0500

    Change Defaults

--- chess_pieces_defaults.py
+++ chess_pieces_defaults.py
@@ -1,107 +1,227 @@
 from chesspiece import ChessPiece
-
+x_off = 50
+y_off = 50
 pieces_dict = {
                 'w_rook_1':ChessPiece(
-                                    init_x = 50,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 0+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_knight_1':ChessPiece(
-                                    init_x = 100,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 50+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_bishop_1':ChessPiece(
-                                    init_x = 150,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 100+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_queen':ChessPiece(
-                                    init_x = 200,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 150+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_king':ChessPiece(
-                                    init_x = 250,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 200+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_bishop_2':ChessPiece(
-                                    init_x = 300,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 250+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_knight_2':ChessPiece(
-                                    init_x = 350,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 300+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_rook_2':ChessPiece(
-                                    init_x = 400,
-                                    init_y = 50,
+                                    init_x = 0+x_off,
+                                    init_y = 350+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_1':ChessPiece(
-                                    init_x = 50,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 0+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_2':ChessPiece(
-                                    init_x = 100,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 50+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'w_pawn_3':ChessPiece(
+                                    init_x = 50+x_off,
+                                    init_y = 100+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_4':ChessPiece(
-                                    init_x = 150,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 150+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_5':ChessPiece(
-                                    init_x = 200,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 200+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_6':ChessPiece(
-                                    init_x = 250,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 250+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_7':ChessPiece(
-                                    init_x = 300,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 300+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   ),
 
                 'w_pawn_8':ChessPiece(
-                                    init_x = 350,
-                                    init_y = 100,
+                                    init_x = 50+x_off,
+                                    init_y = 350+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_rook_1':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 0+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_knight_1':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 50+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_bishop_1':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 100+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_queen':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 150+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_king':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 200+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_bishop_2':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 250+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_knight_2':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 300+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_rook_2':ChessPiece(
+                                    init_x = 350+x_off,
+                                    init_y = 350+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_1':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 0+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_2':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 50+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_3':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 100+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_4':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 150+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_5':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 200+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_6':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 250+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_7':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 300+y_off,
+                                    team = 'white',
+                                    piece_type = 'rook'
+                                  ),
+
+                'b_pawn_8':ChessPiece(
+                                    init_x = 300+x_off,
+                                    init_y = 350+y_off,
                                     team = 'white',
                                     piece_type = 'rook'
                                   )
